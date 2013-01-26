209c27c7c6a55f91244ef4a6917a34d6f1eecea7
Ryan Giglio <ryangiglio@gmail.com>
2013-01-25 20:10:36 -0500

    Add game engine base

---
+++ game.js
@@ -0,0 +1,26 @@
+function update() {
+	
+}
+
+function draw() {
+	
+}
+
+timer.onTick = function (dt) {
+
+	update();
+	draw();
+
+}
+
+input.onMouseDown = function(x, y) {
+	// handle mousedown
+}
+
+input.onMouseUp = function(x, y) {
+	// handle mouseup
+}
+
+input.onMouseMove = function(x, y) {
+	// handle mousemove
+}
