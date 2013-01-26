92c0a40adb7989ef830dc09e74e4cc475456e198
rick <shh@no.lol>
2013-01-25 21:20:07 -0500

    z/z/z/z/z/z/z/z/z/z/z/

--- game.js
+++ game.js
@@ -41,13 +41,29 @@ $(document).ready(function () {
 	function Person() {
 		this.leg = 0;
 		this.walk = false;
+		this.walkCounter = 0;
+		this.walkCheck = false;
+		this.lastTime = new Date();
 		this.update = function () {
+			this.walkCheck = false;
 			if (whichKey == 0 && this.leg == 1) {
 				this.leg = 0;
 				this.walk = true;
+				this.walkCheck = true;
 			} else if (whichKey == 1 && this.leg == 0) {
 				this.leg = 1;
 				this.walk = true;
+				this.walkCheck = true;
+			}
+
+			if (this.walkCheck) {
+				//determine time since last step
+
+				var now = new Date();
+				var elapsed = now - this.lastTime;
+				this.lastTime = now;
+
+				$("#rate").html(elapsed);
 			}
 		}
 	}
